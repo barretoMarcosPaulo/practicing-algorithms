a826c1025910d8b48cc94f5b7c58a62ae825f559
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-04-10 00:06:46 -0300

    refact 04-max-of-four.c

--- c/04-max-of-four.c
+++ c/04-max-of-four.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 
-int max_of_four(int n1, int n2, int n3, int n4){
-    int array[] = {n1,n2,n3,n4};
-    int aux = 0;
-
-    for(int i = 0 ; i < 4; i++){
-        for(int j = 0 ; j < 4; j++){
-            if(array[i] < array[j]){
-                aux=array[i];
-                array[i] = array[j];
-                array[j] = aux;
-
-            }
-        }
+int max_of_four(int a, int b, int c, int d){
+    int numbers[]={b,c,d};
+    int max = a;
+    for(int i = 0 ; i < 3 ; i++){
+        if(max < numbers[i]) max = numbers[i];
     }
+    return max;
 
-    return array[3];
 }
+
 
 int main() {
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
-    printf("%d\n", ans);
+    printf("%d", ans);
     
     return 0;
 }
